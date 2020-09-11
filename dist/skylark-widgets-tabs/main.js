f2072fd99bf398fc0e0d30a5115fa31a2eae31dc
hudaokeji <github@hudaokeji.com>
2020-09-11 21:02:54 +0900

    update

--- dist/skylark-widgets-tabs/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-widgets-tabs - The skylark tabs widgets library
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-widgets/skylark-widgets-tabs/
- * @license MIT
- */
-define(["./tabs","./TabButton","./TabElement","./TabGroup","./splittable/TabButtonSplit","./splittable/TabContainer","./splittable/TabDualContainer","./splittable/TabGroupSplit"],function(t){return t});
-//# sourceMappingURL=sourcemaps/main.js.map
